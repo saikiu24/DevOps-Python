5daf1ddff996a34a08e401fab915ce0121235529
phoenixyork166 <berlinlee2017@gmail.com>
2024-01-01 20:06:36 +0800

    commit

---
+++ Desktop/Web/vscode/Python/variables.py
@@ -0,0 +1,26 @@
+user_iq = 190
+
+print(user_iq)
+
+#variables must start with lowercase
+#variables are case-sensitive
+
+#_user_iq = private variables
+_user_iq = 180
+print('_user_iq:', _user_iq)
+user_age = _user_iq/4
+print('user_age:', user_age)
+
+#constants
+#all capitals tell others this is a const
+PI = 3.14
+print('PI:', PI)
+
+# Dunder variables
+# __
+# We should NOT create Dunders __
+a,b,c = 1,2,3
+print('a,b,c:', a,b,c)
+print('a:', a)
+print('b:', b)
+print('c:', c)
